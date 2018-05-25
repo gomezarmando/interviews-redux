5ce3c9551cc31683827d497b3ce8a22cae91e3c0
Luis Armando Gomez <luisgomez@MacBook-Pro.local>
2018-05-24 23:52:00 -0500

    Instantiating List component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {InterviewList} from './Components';
+
+const userInterviews = [
+  {id: 1, name: 'HBO interview'},
+  {id: 2, name: 'GOOGLE interview'}
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InterviewList
+          items={userInterviews} />
       </div>
     );
   }
